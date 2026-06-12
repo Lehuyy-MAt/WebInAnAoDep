d7b24098fba898e933cfbe4134764d952d01d2af
Lehuyy-MAt <uchizuna2004bn@gmail.com>
2026-06-13 05:32:02 +0700

    Fix PostgreSQL lower bytea error

--- src/main/java/com/example/webinanao/config/CloudinaryConfig.java
+++ src/main/java/com/example/webinanao/config/CloudinaryConfig.java
@@ -6,25 +6,29 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class CloudinaryConfig {
 
-    @Value("${cloudinary.cloud_name}")
+    @Value("${CLOUDINARY_CLOUD_NAME}")
     private String cloudName;
 
-    @Value("${cloudinary.api_key}")
+    @Value("${CLOUDINARY_API_KEY}")
     private String apiKey;
 
-    @Value("${cloudinary.api_secret}")
+    @Value("${CLOUDINARY_API_SECRET}")
     private String apiSecret;
 
     @Bean
     public Cloudinary cloudinary() {
-        return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", cloudName,
-                "api_key", apiKey,
-                "api_secret", apiSecret,
-                "secure", true
-        ));
+        Map<String, String> config = new HashMap<>();
+
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+
+        return new Cloudinary(config);
     }
 }
